4a72d5c96000a8a2905b894863e556c3510222fa
Anar <53255824+Anar-88@users.noreply.github.com>
2020-06-25 19:55:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ $(function (){
     let btnFunctions = "<li><span class='shopping-item'>" + text + "</span> <div class='shopping-item-controls'> <button class='shopping-item-toggle'> <span class='button-label'>check</span> </button> <button class='shopping-item-delete'> <span class='button-label'>delete</span> </button> </div> </li>"
    $("#shopping-list-entry").val('');
   $("ul").append(btnFunctions);
-  
   });
 
-  $(document).on('click', '.shopping-item-toggle', function (event){
-  $(this).closest("li").toggleClass("shopping-item__checked");
+  $('.shopping-item-toggle').on('click', function (event){
+  $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
   });
 
-  $(document).on('click', ".shopping-item-delete", function(event){
+  $(".shopping-item-delete").on('click', function(event){
   $(this).closest("li").remove();
  });
 });
